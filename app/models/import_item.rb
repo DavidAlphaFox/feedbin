287b0dcc8841ea5c6abb6e86431d93f9b500895a
Ben Ubois <ben@benubois.com>
2022-10-08 23:16:32 +0200

    store -> store_accessor

--- app/models/import_item.rb
+++ app/models/import_item.rb
@@ -5,7 +5,7 @@ class ImportItem < ApplicationRecord
   after_commit :import_feed, on: :create
 
   enum status: [:pending, :complete, :failed]
-  store :error, accessors: [:class, :message], coder: JSON, prefix: true
+  store_accessor :error, :class, :message, prefix: true
 
   def import_feed
     FeedImporter.perform_async(id)
