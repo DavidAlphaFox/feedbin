09baf90f11272e075754f1484cb675b1dbbf3865
Ben Ubois <ben@benubois.com>
2023-01-24 16:37:16 +0100

    Update control groups.

--- app/components/settings/control_group_component.rb
+++ app/components/settings/control_group_component.rb
@@ -7,6 +7,12 @@ class Settings::ControlGroupComponent < BaseComponent
     @options = options
   end
 
+  def options
+    {
+      class: [@options.delete(:class)].reject(&:blank?).join(" "),
+    }.merge(@options)
+  end
+
   class ItemComponent < BaseComponent
     def initialize(options = {})
       @options = options
@@ -23,7 +29,7 @@ class Settings::ControlGroupComponent < BaseComponent
     private
 
     def classes
-      "border-b last:border-b-0 group-data-[borderless=true]:border-b-0"
+      "border-b last:border-b-0"
     end
 
     def data
